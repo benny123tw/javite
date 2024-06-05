41e951b5812474fa928b20775db870802eef53de
Benny Yen <benny123tw@gmail.com>
2024-06-05 18:09:44 +0800

    feat(spring-mvc): add vite JSP tag - import

---
+++ spring-webmvc/src/main/java/io/github/benny123tw/servlet/config/ViteProperties.java
@@ -0,0 +1,45 @@
+package io.github.benny123tw.servlet.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "vite")
+public class ViteProperties {
+
+    private boolean debug = true;
+    private String manifestPath = "/WEB-INF/dist/.vite/manifest.json";
+    private String localServerUrl = "http://localhost:5173";
+    private String resourcePath = "/resources";
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    public String getManifestPath() {
+        return manifestPath;
+    }
+
+    public void setManifestPath(String manifestPath) {
+        this.manifestPath = manifestPath;
+    }
+
+    public String getLocalServerUrl() {
+        return localServerUrl;
+    }
+
+    public void setLocalServerUrl(String localServerUrl) {
+        this.localServerUrl = localServerUrl;
+    }
+
+    public String getResourcePath() {
+        return resourcePath;
+    }
+
+    public void setResourcePath(String resourcePath) {
+        this.resourcePath = resourcePath;
+    }
+
+}
---
+++ spring-webmvc/src/main/java/io/github/benny123tw/servlet/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Defines the XML configuration namespace for Spring MVC.
+ */
+@NonNullApi
+@NonNullFields
+package io.github.benny123tw.servlet;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
