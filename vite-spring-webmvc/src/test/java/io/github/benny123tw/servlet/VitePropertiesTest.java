88e0365220d9f3eb05a35f01b5db273da3d3bcf5
Benny Yen <benny123tw@gmail.com>
2024-06-07 11:06:37 +0800

    chore(vite-spring-mvc): rename package

--- vite-spring-webmvc/src/test/java/io/github/benny123tw/servlet/VitePropertiesTest.java
+++ vite-spring-webmvc/src/test/java/io/github/benny123tw/servlet/VitePropertiesTest.java
@@ -1,13 +1,13 @@
 package io.github.benny123tw.servlet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import io.github.benny123tw.servlet.config.ViteProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest(classes = TestApplication.class)
 @ActiveProfiles("test")
