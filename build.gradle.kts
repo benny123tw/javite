d4df37e83de3b762071002e2bf0ced6c83766118
Benny Yen <benny123tw@gmail.com>
2024-06-11 00:33:05 +0800

    chore: migrate to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     `java-library`
-    alias(libs.plugins.publish)
-    alias(libs.plugins.release)
+    alias(libs.plugins.publish.maven)
 }
 
 description = "Vite Integration"
@@ -13,25 +14,55 @@ allprojects {
     }
 }
 
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            val ossrhUsername = providers.environmentVariable("OSSRH_USERNAME")
-            val ossrhPassword = providers.environmentVariable("OSSRH_PASSWORD")
-            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
-                username.set(ossrhUsername.get())
-                password.set(ossrhPassword.get())
-            }
-        }
-    }
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
 
-// do not generate extra load on Nexus with new staging repository if signing fails
-val initializeSonatypeStagingRepository by tasks.existing
-subprojects {
-    initializeSonatypeStagingRepository {
-        shouldRunAfter(tasks.withType<Sign>())
+object Meta {
+    const val RELEASE = "https://s01.oss.sonatype.org/service/local/"
+    const val SNAPSHOT = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+    const val DESC = "Vite integration with Spring Web MVC."
+    const val LICENSE = "MIT"
+    const val LICENSE_URL = "https://opensource.org/licenses/mit"
+    const val GITHUB_REPO = "benny123tw/vite-integration"
+    const val DEVELOPER_ID = "benny123tw"
+    const val DEVELOPER_NAME = "Benny Yen"
+    const val DEVELOPER_ORGANIZATION = "io.github.benny123tw"
+    const val DEVELOPER_ORGANIZATION_URL = "https://www.github.com/benny123tw"
+}
+
+mavenPublishing {
+    println("Publish ${project.group.toString()}, ${project.name}, ${project.version.toString()}")
+    coordinates(project.group.toString(), project.name, project.version.toString())
+
+    pom {
+        name.set(project.name)
+        description.set(Meta.DESC)
+        inceptionYear.set("2024")
+        url.set(Meta.GITHUB_REPO)
+        licenses {
+            license {
+                name.set(Meta.LICENSE)
+                url.set(Meta.LICENSE_URL)
+            }
+        }
+        developers {
+            developer {
+                id.set(Meta.DEVELOPER_ID)
+                name.set(Meta.DEVELOPER_NAME)
+                organization.set(Meta.DEVELOPER_ORGANIZATION)
+                organizationUrl.set(Meta.DEVELOPER_ORGANIZATION_URL)
+            }
+        }
+        scm {
+            url.set("https://github.com/${Meta.GITHUB_REPO}.git")
+            connection.set("scm:git:git://github.com/${Meta.GITHUB_REPO}.git")
+            developerConnection.set("scm:git:git://github.com/${Meta.GITHUB_REPO}.git")
+        }
+        issueManagement {
+            url.set("https://github.com/${Meta.GITHUB_REPO}/issues")
+        }
     }
 }
--- vite-spring-webmvc/build.gradle.kts
+++ vite-spring-webmvc/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SonatypeHost
+
 object Meta {
     const val RELEASE = "https://s01.oss.sonatype.org/service/local/"
     const val SNAPSHOT = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-    const val DESC = "OSS GitHub Java Library Template Repository"
+    const val DESC = "Vite integration with Spring Web MVC."
     const val LICENSE = "MIT"
     const val LICENSE_URL = "https://opensource.org/licenses/mit"
     const val GITHUB_REPO = "benny123tw/vite-integration"
@@ -17,6 +21,7 @@ plugins {
     signing
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.publish.maven)
 }
 
 description = "Vite - Spring Web MVC"
@@ -39,59 +44,19 @@ tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+}
+
 tasks.bootJar {
     enabled = false
 }
 
-signing {
-    val signingKey = providers.environmentVariable("GPG_SIGNING_KEY")
-    val signingPassphrase = providers.environmentVariable("GPG_SIGNING_PASSPHRASE")
-    if (signingKey.isPresent && signingPassphrase.isPresent) {
-        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
-        val extension = extensions.getByName("publishing") as PublishingExtension
-        sign(extension.publications)
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["java"])
-            pom {
-                name.set(project.name)
-                description.set(Meta.DESC)
-                url.set("https://github.com/${Meta.GITHUB_REPO}")
-                licenses {
-                    license {
-                        name.set(Meta.LICENSE)
-                        url.set(Meta.LICENSE_URL)
-                    }
-                }
-                developers {
-                    developer {
-                        id.set(Meta.DEVELOPER_ID)
-                        name.set(Meta.DEVELOPER_NAME)
-                        organization.set(Meta.DEVELOPER_ORGANIZATION)
-                        organizationUrl.set(Meta.DEVELOPER_ORGANIZATION_URL)
-                    }
-                }
-                scm {
-                    url.set("https://github.com/${Meta.GITHUB_REPO}.git")
-                    connection.set("scm:git:git://github.com/${Meta.GITHUB_REPO}.git")
-                    developerConnection.set("scm:git:git://github.com/${Meta.GITHUB_REPO}.git")
-                }
-                issueManagement {
-                    url.set("https://github.com/${Meta.GITHUB_REPO}/issues")
-                }
-            }
-        }
-    }
-}
-
 tasks.jar {
+    enabled = true
+    archiveClassifier.set("")
     manifest {
         attributes(
             mapOf(
@@ -102,15 +67,58 @@ tasks.jar {
     }
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
+mavenPublishing {
+    println("Publish ${project.group}, ${project.name}, ${project.version}")
+    coordinates(project.group.toString(), project.name, project.version.toString())
+
+    // Correctly configure JavaLibrary with Javadoc and sources JARs
+    configure(
+        JavaLibrary(
+            javadocJar = JavadocJar.Javadoc(),
+            sourcesJar = true
+        )
+    )
+
+    pom {
+        name.set(project.name)
+        description.set(Meta.DESC)
+        inceptionYear.set("2024")
+        url.set(Meta.GITHUB_REPO)
+        licenses {
+            license {
+                name.set(Meta.LICENSE)
+                url.set(Meta.LICENSE_URL)
+            }
+        }
+        developers {
+            developer {
+                id.set(Meta.DEVELOPER_ID)
+                name.set(Meta.DEVELOPER_NAME)
+                organization.set(Meta.DEVELOPER_ORGANIZATION)
+                organizationUrl.set(Meta.DEVELOPER_ORGANIZATION_URL)
+            }
+        }
+        scm {
+            url.set("https://github.com/${Meta.GITHUB_REPO}.git")
+            connection.set("scm:git:git://github.com/${Meta.GITHUB_REPO}.git")
+            developerConnection.set("scm:git:git://github.com/${Meta.GITHUB_REPO}.git")
+        }
+        issueManagement {
+            url.set("https://github.com/${Meta.GITHUB_REPO}/issues")
+        }
+    }
 }
+
+// Ensure that generateMetadataFileForMavenPublication depends on the javadocJar task
+//tasks.named("generateMetadataFileForMavenPublication") {
+//    dependsOn(tasks.named("javadocJar"))
+//}
 
 // gradle locking of dependency versions
 //   *required+used for trivy scan
 dependencyLocking {
     lockAllConfigurations()
 }
+
 // always run subproject task with parent
 rootProject.tasks.dependencies { dependsOn(tasks.dependencies) }
