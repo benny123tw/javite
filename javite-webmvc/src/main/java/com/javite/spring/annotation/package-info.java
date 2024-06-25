69db17fa4cdfe969c2dd3adc9d605031c8e31960
Benny Yen <benny123tw@gmail.com>
2024-06-25 15:40:18 +0800

    feat: add new module javite-webmvc

---
+++ javite-webmvc/src/main/java/com/javite/spring/annotation/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains annotations for enabling Vite integration in Spring MVC applications.
+ *
+ * <p>The primary annotation in this package is {@link com.javite.spring.annotation.EnableVite}, which when applied to a
+ * configuration class, imports the necessary configuration for Vite.</p>
+ *
+ * @see com.javite.spring.config.ViteConfig
+ */
+package com.javite.spring.annotation;
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "javite"
 include("vite-spring-webmvc")
 include("vite-spring-webmvc-jre8")
 include("javite-core")
+include("javite-webmvc")
