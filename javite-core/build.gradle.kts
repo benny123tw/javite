180a06dd5bb2a139cd2c472c96772aa9ec3383f1
Benny Yen <benny123tw@gmail.com>
2024-06-25 18:01:31 +0800

    chore(javite-core): downgrade to 8

--- javite-core/build.gradle.kts
+++ javite-core/build.gradle.kts
@@ -7,7 +7,13 @@ plugins {
     alias(libs.plugins.publish.maven)
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     implementation(libs.jackson.databind)
 
     testImplementation(platform(libs.junit.bom))
